69730272b08568bed4e6fb16938f99df62a41e13
Aaron Cronin <cronin@include.cat>
2013-11-14 11:53:19 +0000

    Frame function complete

--- Strings/frames.cpp
+++ Strings/frames.cpp
@@ -7,7 +7,30 @@ str_size_t width(const vector<string>& v) {
   str_size_t longest = 0;
 
   typedef vector<string>::const_iterator iter;
-  for(iter it = v.begin(); it != v.end(); ++it) longest = max(longest, it->size());
+  for (iter it = v.begin(); it != v.end(); ++it) longest = max(longest, it->size());
 
   return longest;
+}
+
+vector<string> frame(
+  const vector<string>& v,
+  const char c,
+  const int pad
+) {
+  vector<string> ret;
+  str_size_t longest = width(v);
+  string border(longest + (2 * pad) + 2, c);
+
+  ret.push_back(border);
+
+  typedef vector<string>::const_iterator iter;
+  for (iter it = v.begin(); it != v.end(); ++it) {
+    int trailing = longest + pad - it->size();
+    string line = c + string(pad, ' ') + *it + string(trailing, ' ') + c;
+    ret.push_back(line);
+  }
+
+  ret.push_back(border);
+
+  return ret;
 }
--- Strings/frames.h
+++ Strings/frames.h
@@ -7,4 +7,11 @@
 // Length of longest string in vector
 std::string::size_type width(const std::vector<std::string>&);
 
+// Frame the vector<string> by surrounding in given char
+std::vector<std::string> frame(
+  const std::vector<std::string>& v,
+  const char c,
+  const int pad
+);
+
 #endif
